0feb4e03600594d738e398ee7d40b2b8d4a8d61a
Daniel Sandu <3251086-dansandu@users.noreply.gitlab.com>
2019-05-18 18:28:12 +0300

    Add external libraries path to unit tests environment

--- sources/praline/client/project/pipeline/stage/run_test_executable.py
+++ sources/praline/client/project/pipeline/stage/run_test_executable.py
@@ -2,7 +2,7 @@ from praline.client.project.pipeline.stage.base import stage
 from praline.common.file_system import execute_and_fail_on_bad_return, exists
 
 
-@stage(consumes=['test_executable'], produces=['test_results'], exposed=True)
+@stage(consumes=['external_libraries_root', 'test_executable'], produces=['test_results'], exposed=True)
 def run_test_executable(working_directory, data, cache, arguments):
-    execute_and_fail_on_bad_return([data['test_executable']])
+    execute_and_fail_on_bad_return([data['test_executable']], add_to_path=[data['external_libraries_root']])
     data['test_results'] = 'success'
