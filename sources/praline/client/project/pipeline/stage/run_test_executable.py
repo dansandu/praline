9b17f6c81a2ae06d244a3f6dc810a2f054611a92
Daniel Sandu <3251086-dansandu@users.noreply.gitlab.com>
2019-06-14 18:25:09 +0000

    Executable stage arguments forwarding

--- sources/praline/client/project/pipeline/stage/run_test_executable.py
+++ sources/praline/client/project/pipeline/stage/run_test_executable.py
@@ -1,8 +1,20 @@
+from praline.client.project.pipeline.arguments import pass_remainder_arguments
 from praline.client.project.pipeline.stage.base import stage
 from praline.common.file_system import execute_and_fail_on_bad_return, exists
 
 
-@stage(consumes=['external_libraries_root', 'test_executable'], produces=['test_results'], exposed=True)
+parameters = [
+    {
+        'name': '--arguments',
+        'action': 'store',
+        'nargs': pass_remainder_arguments,
+        'dest': 'arguments',
+        'help': 'Forward arguments to the underlying executable being run. Be warned that all proceeding arguments are forwarded and no longer used by praline!',
+        'default': []
+    }
+]
+
+@stage(consumes=['external_libraries_root', 'test_executable'], produces=['test_results'], exposed=True, parameters=parameters)
 def run_test_executable(working_directory, data, cache, arguments):
-    execute_and_fail_on_bad_return([data['test_executable']], add_to_path=[data['external_libraries_root']])
+    execute_and_fail_on_bad_return([data['test_executable']] + arguments['arguments'], add_to_path=[data['external_libraries_root']])
     data['test_results'] = 'success'
